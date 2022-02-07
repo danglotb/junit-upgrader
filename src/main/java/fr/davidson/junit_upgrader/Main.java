fea06c7d65fd8cb3c7c1b2f3cf532adafb8687c8
danglotb <benjamin.danglot@davidson.fr>
2022-02-07 17:09:39 +0100

    feat: improve CLI

--- src/main/java/fr/davidson/junit_upgrader/Main.java
+++ src/main/java/fr/davidson/junit_upgrader/Main.java
@@ -9,20 +9,44 @@ import spoon.Launcher;
  * benjamin.danglot@davidson.fr
  * on 07/02/2022
  */
+@CommandLine.Command(name = "fr.davidson.junit_upgrader.Main", mixinStandardHelpOptions = true)
 public class Main {
 
     @CommandLine.Option(
             names = "--input-path",
-            description = "Specify the path to the folder that contains the test code to be processed."
+            description = "Specify the path to the folder that contains the test code to be processed.",
+            required = true
     )
-    private static String inputPath;
+    private String inputPath;
 
-    public static void main(String[] args) {
+    public void run() {
         Launcher launcher = new Launcher();
-        launcher.addInputResource(inputPath);
+        launcher.addInputResource(this.inputPath);
         launcher.getEnvironment().setNoClasspath(true);
         launcher.addProcessor(new JUnitUpgraderProcessor());
         launcher.run();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        final CommandLine commandLine = new CommandLine(main);
+        commandLine.setUsageHelpWidth(120);
+        try {
+            commandLine.parseArgs(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+            commandLine.usage(System.err);
+            System.exit(11);
+        }
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            System.exit(11);
+        }
+        if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+            System.exit(11);
+        }
+        main.run();
     }
 
 }
